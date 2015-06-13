45a5daed0ae9526fb729dedc55c6e52f1ebfdbd9
Michael Pascuzzi <gatorpazz@gmail.com>
2015-06-13 15:44:40 -0400

    Fixed the JS files

--- src/js/main.js
+++ src/js/main.js
@@ -34,7 +34,7 @@
                     }
                 };
             }
-
+          }
 
             $('#tab2').click(function() {
                 $('.hide').addClass('hidden');
@@ -71,61 +71,6 @@
             function upgrade() {
                 alert("This is 2015! Get Google Chrome and browse the web in style!");
             }
-
-            (function($, undefined) {
-                $.fn.getCursorPosition = function() {
-                    var el = $(this).get(0);
-                    var pos = 0;
-                    if ('selectionStart' in el) {
-                        pos = el.selectionStart;
-                    } else if ('selection' in document) {
-                        el.focus();
-                        var Sel = document.selection.createRange();
-                        var SelLength = document.selection.createRange().text.length;
-                        Sel.moveStart('character', -el.value.length);
-                        pos = Sel.text.length - SelLength;
-                    }
-                    return pos;
-                };
-                $.fn.setCursorPosition = function(pos) {
-                    if (this.setSelectionRange) {
-                        this.setSelectionRange(pos, pos);
-                    } else if (this.createTextRange) {
-                        var range = this.createTextRange();
-                        range.collapse(true);
-                        if (pos < 0) {
-                            pos = $(this).val().length + pos;
-                        }
-                        range.moveEnd('character', pos);
-                        range.moveStart('character', pos);
-                        range.select();
-                    }
-                };
-                $.fn.insertAtCaret = function(myValue) {
-                    return this.each(function() {
-                        var me = this;
-                        if (document.selection) { // IE
-                            me.focus();
-                            sel = document.selection.createRange();
-                            sel.text = myValue;
-                            me.focus();
-                        } else if (me.selectionStart || me.selectionStart == '0') { // Real browsers
-                            var startPos = me.selectionStart,
-                                endPos = me.selectionEnd,
-                                scrollTop = me.scrollTop;
-                            me.value = me.value.substring(0, startPos) + myValue + me.value.substring(endPos, me.value.length);
-                            me.focus();
-                            me.selectionStart = startPos + myValue.length;
-                            me.selectionEnd = startPos + myValue.length;
-                            me.scrollTop = scrollTop;
-                        } else {
-                            me.value += myValue;
-                            me.focus();
-                        }
-                    });
-                };
-            })(jQuery);
-        })(window);
 
     (function($, undefined) {
         $.fn.getCursorPosition = function() {
